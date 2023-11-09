a5695d9c2a63acabbbbfd7337da12ae5ee3b2588
Aleksa Matic <aleksa.matic10@gmail.com>
2023-11-09 19:47:19 +0100

    Add Error Page

--- jobify-fe/src/pages/Error.tsx
+++ jobify-fe/src/pages/Error.tsx
@@ -3,9 +3,24 @@ import Wrapper from "../assets/wrappers/ErrorPage";
 import notFound from "../assets/images/not-found.svg";
 
 const Error: React.FC = () => {
-  const error: unknown = useRouteError();
+  const error: any = useRouteError();
 
-  console.log(error);
+  if (!error) {
+    return;
+  }
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={notFound} alt="not found" />
+          <h3>Cannot find the page you are looking for</h3>
+          <p>It seems the page does not exist</p>
+          <Link to="/dashboard">Dashboard</Link>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
     <Wrapper>
       <div>
