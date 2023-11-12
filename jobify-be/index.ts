4902de8e49aa59090d43985437c8dc7f59a3950c
Aleksa Matic <aleksa.matic10@gmail.com>
2023-11-12 17:50:18 +0100

    Make Get All Jobs Route

--- jobify-be/index.ts
+++ jobify-be/index.ts
@@ -1,13 +1,43 @@
-import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
 
+import express, { Express, Request, Response } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { nanoid } from "nanoid";
+
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5100;
+
+let jobs = [
+  {
+    id: nanoid(),
+    company: "apple",
+    position: "front-end",
+  },
+  {
+    id: nanoid(),
+    company: "samsung",
+    position: "backend-end",
+  },
+];
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
+});
+
+app.get("/api/v1/jobs", (req: Request, res: Response) => {
+  res.status(200).json(jobs);
+});
+
+app.post("/", (req: Request, res: Response) => {
+  res.json({ message: "data received", data: req.body });
 });
 
 app.listen(port, () => {
