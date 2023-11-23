ead0e4753de4c02f7891bee4d82dff5db2a72dfd
Aleksa Matic <aleksa.matic10@gmail.com>
2023-11-23 19:12:30 +0100

    register user

--- jobify-be/controller/authController.ts
+++ jobify-be/controller/authController.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
+import User from "../models/UserModel.js";
+import { UserBackendModel } from "../types/index.js";
+import { StatusCode } from "../enum/index.js";
 
 export const registerController = async (req: Request, res: Response) => {
-  res.send("Register");
+  const user = await User.create(req.body);
+  res.status(StatusCode.CREATED).send({ user });
 };
 
 export const loginController = async (req: Request, res: Response) => {
