a795946ecd8a3e01e9e9d299660eab738efa2d11
Aleksa Matic <aleksa.matic10@gmail.com>
2023-11-25 00:46:14 +0100

    Implement password comparation

--- jobify-be/utils/passwordUtils.ts
+++ jobify-be/utils/passwordUtils.ts
@@ -1,6 +1,13 @@
 import bcrypt from "bcryptjs";
 
-export async function hashPassword(password: string) {
+export async function hashPassword(password: string): Promise<string> {
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
+}
+
+export async function comparePasswords(
+  password: string,
+  hashedPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, hashedPassword);
 }
