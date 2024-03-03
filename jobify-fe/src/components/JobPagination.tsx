285609fb73987be61a7680f6a20cb3995182e441
Aleksa Matic <aleksa.matic10@gmail.com>
2024-03-03 21:51:23 +0100

    dynamic pagination

--- jobify-fe/src/components/JobPagination.tsx
+++ jobify-fe/src/components/JobPagination.tsx
@@ -12,13 +12,64 @@ const JobPagination: React.FC = () => {
   const { pathname, search } = useLocation();
   const navigate = useNavigate();
 
-  const pageBtns = Array.from({ length: totalPages }, (_, index) => index + 1);
-
   const handlePageChange = (page: number) => {
     const searchParams: URLSearchParams = new URLSearchParams(search);
-    console.log(searchParams);
     searchParams.set("page", String(page));
     navigate(`${pathname}?${searchParams.toString()}`);
+  };
+
+  const renderButton = (pageNumber: number, isActive: boolean) => {
+    return (
+      <button
+        key={pageNumber}
+        className={`btn page-btn ${isActive && "active"}`}
+        onClick={() => handlePageChange(pageNumber)}
+      >
+        {pageNumber}
+      </button>
+    );
+  };
+
+  const renderButtons = () => {
+    const buttons = [];
+
+    // FIRST PAGE
+    buttons.push(renderButton(1, currentPage === 1));
+
+    if (currentPage > 3) {
+      buttons.push(
+        <span className="btn page-btn" key="dots-1">
+          ...
+        </span>
+      );
+    }
+
+    // PREVIOUS PAGE
+    if (currentPage !== 1 && currentPage !== 2) {
+      buttons.push(renderButton(currentPage - 1, false));
+    }
+    // CURRENT PAGE
+    if (currentPage !== 1 && currentPage !== totalPages) {
+      buttons.push(renderButton(currentPage, true));
+    }
+
+    // NEXT PAGE
+    if (currentPage !== totalPages && currentPage !== totalPages - 1) {
+      buttons.push(renderButton(currentPage + 1, false));
+    }
+
+    if (currentPage < totalPages - 2) {
+      buttons.push(
+        <span className="btn page-btn" key="dots-2">
+          ...
+        </span>
+      );
+    }
+
+    // LAST PAGE
+    buttons.push(renderButton(totalPages, currentPage === totalPages));
+
+    return buttons;
   };
 
   return (
@@ -35,17 +86,7 @@ const JobPagination: React.FC = () => {
         prev
       </button>
 
-      <div className="btn-container">
-        {pageBtns.map((pageNumber) => (
-          <button
-            key={pageNumber}
-            className={`btn page-btn ${currentPage === pageNumber && "active"}`}
-            onClick={() => handlePageChange(pageNumber)}
-          >
-            {pageNumber}
-          </button>
-        ))}
-      </div>
+      <div className="btn-container">{renderButtons()}</div>
 
       <button
         className="btn next-btn"
