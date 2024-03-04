4aff5081d3b008e9e8005dee309b68e31492a326
Aleksa Matic <aleksa.matic10@gmail.com>
2024-03-04 23:45:14 +0100

    add create button to all-jobs page

--- jobify-fe/src/assets/wrappers/JobsContainer.ts
+++ jobify-fe/src/assets/wrappers/JobsContainer.ts
@@ -14,6 +14,21 @@ const Wrapper = styled.section`
     grid-template-columns: 1fr 1fr;
     gap: 1rem;
   }
+  .form-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-bottom: 0.5rem;
+    margin-bottom: 1rem;
+    border-bottom: 1px solid var(--grey-400);
+  }
+  .header-btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1.2rem 4rem;
+    font-size: 1.2rem;
+  }
   @media (min-width: 1120px) {
     .jobs {
       grid-template-columns: 1fr 1fr;
--- jobify-fe/src/components/JobsContainer.tsx
+++ jobify-fe/src/components/JobsContainer.tsx
@@ -3,8 +3,10 @@ import Wrapper from "../assets/wrappers/JobsContainer";
 import { JobData, JobsWithPagination } from "../types";
 import Job from "./Job";
 import { JobPagination } from ".";
+import { useNavigate } from "react-router-dom";
 
 const JobsContainer: React.FC = () => {
+  const navigation = useNavigate();
   const { jobs, pagination } = useAllJobContext() as JobsWithPagination;
 
   if (jobs.length === 0) {
@@ -17,7 +19,17 @@ const JobsContainer: React.FC = () => {
 
   return (
     <Wrapper>
-      <h5>Total: {pagination.totalJobs}</h5>
+      <div className="form-header">
+        <h5>Total: {pagination.totalJobs}</h5>
+        <button
+          className="btn form-btn header-btn"
+          onClick={() => {
+            navigation("/dashboard");
+          }}
+        >
+          Create job
+        </button>
+      </div>
       <div className="jobs">
         {jobs.map((job: JobData) => {
           return <Job key={job._id} {...job} />;
